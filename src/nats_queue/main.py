e129159e6643d3d66288628985bf0453bdb2f3cc
Kristina Shishkina <k.shishkina@darwinsoft.ru>
2024-09-23 22:36:39 +0300

    commit

--- src/nats_queue/main.py
+++ src/nats_queue/main.py
@@ -5,9 +5,17 @@ import uuid
 import json
 import time
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+logging.basicConfig(
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    level=logging.DEBUG
+)
 
 logger = logging.getLogger('nats')
-logging.basicConfig(level=logging.DEBUG)
+
 user = os.environ.get('NATS_USER')
 password = os.environ.get('NATS_PASSWORD')
 
@@ -19,10 +27,11 @@ class Job:
         self.data = data
         self.delay = delay
         self.meta = meta or {}
+        self.retry_count = 0
 
     @property
     def subject(self):
-        return f"{self.queue_name}.{self.name}"  # Основной сабжект без приоритета
+        return f"{self.queue_name}.{self.name}"
 
     def to_dict(self):
         return {
@@ -30,7 +39,8 @@ class Job:
             "name": self.name,
             "data": self.data,
             "meta": self.meta,
-            "delay": self.delay
+            "delay": self.delay,
+            "retry_count": self.retry_count
         }
 
 class Queue:
@@ -47,14 +57,12 @@ class Queue:
             self.js = self.nc.jetstream()
             logger.info("Успешно подключено к NATS")
 
-            # Удаление старого стрима (если необходимо)
             try:
                 await self.js.delete_stream(self.topic_name)
                 logger.info(f"Старый стрим '{self.topic_name}' успешно удален.")
             except Exception as e:
                 logger.warning(f"Ошибка при удалении стрима '{self.topic_name}': {str(e)}")
 
-            # Создание нового стрима с поддержкой формата {queueName}.{jobName}.{priority}
             subjects = [f"{self.topic_name}.*.*"]
             await self.js.add_stream(name=self.topic_name, subjects=subjects)
         except Exception as e:
@@ -78,8 +86,32 @@ class Queue:
         for job in jobs:
             await self.addJob(job, priority)
 
+class RateLimiter:
+    def __init__(self, max_tasks, duration):
+        self.max_tasks = max_tasks
+        self.duration = duration
+        self.processed_count = 0
+        self.start_time = time.time()
+        logger.debug(f"RateLimiter создан с max_tasks={max_tasks}, duration={duration}.")
+
+    def increment(self):
+        self.processed_count += 1
+        logger.debug(f"Увеличение счетчика обработанных задач: {self.processed_count}.")
+
+    async def check_limit(self):
+        current_time = time.time()
+        elapsed = current_time - self.start_time
+        
+        if elapsed < self.duration and self.processed_count >= self.max_tasks:
+            wait_time = self.duration - elapsed
+            logger.info(f"Достижение лимита обработки. Ожидание {wait_time:.2f} секунд.")
+            await asyncio.sleep(wait_time)
+            self.start_time = time.time()
+            self.processed_count = 0
+            logger.debug(f"Состояние сброшено: start_time обновлен, processed_count сброшен.")
+
 class Worker:
-    def __init__(self, topic_name: str, concurrency: int, rate_limit: dict, processor_callback, priorities: int = 1):
+    def __init__(self, topic_name: str, concurrency: int, rate_limit: dict, processor_callback, priorities: int = 1, max_retries: int = 3):
         self.topic_name = topic_name
         self.concurrency = concurrency
         self.rate_limit = rate_limit
@@ -87,10 +119,10 @@ class Worker:
         self.priorities = priorities
         self.nc = None
         self.js = None
-        self.pending_tasks = set()
-        self.processed_jobs = set()
-        self.last_process_time = time.time()
-        self.semaphore = asyncio.Semaphore(concurrency)
+        self.queue = asyncio.Queue()
+        self.max_retries = max_retries
+        self.semaphore = asyncio.Semaphore(concurrency)  # Используем семафор для контроля параллелизма
+        self.processing_jobs = set() 
 
     async def connect(self):
         logger.info("Подключение воркера к NATS...")
@@ -108,97 +140,104 @@ class Worker:
             await self.nc.close()
             logger.info("Воркер успешно отключен")
 
-    async def _process_task(self, msg, timeout=10):
-        async with self.semaphore:
+    async def _process_task(self, job_data, msg):
+        async with self.semaphore:  # Захватываем семафор
             try:
-                logger.info(f"Обработка задачи {msg.data.decode()}")
-                task = asyncio.create_task(self.processor_callback(msg))
-                self.pending_tasks.add(task)
-                task.add_done_callback(self.pending_tasks.discard)
-                await asyncio.wait_for(task, timeout=timeout)
+                logger.info(f"Обработка задачи {job_data['name']}")
+
+                if job_data.get('retry_count', 0) >= self.max_retries:
+                    logger.error(f"Максимальное количество попыток для задачи {job_data['name']} превышено.")
+                    return
+
+                timeout = self.rate_limit['duration']
+                await asyncio.wait_for(self.processor_callback(job_data), timeout=timeout)
                 await msg.ack()
+                logger.info(f"Задача {job_data['name']} успешно обработана.")
+                msg_id = msg.headers.get("Nats-Msg-Id", None)
+                self.processing_jobs.remove(msg_id)  # Удаляем идентификатор после успешной обработки
+
             except asyncio.TimeoutError:
-                logger.error(f"Тайм-аут задачи {msg.data.decode()}. Повторная отправка.")
-                await msg.nak(delay=5)
+                job_data['retry_count'] += 1
+                logger.error(f"Тайм-аут задачи {job_data['name']}. Попытка {job_data['retry_count']}.")
+                await self.queue.put((job_data, msg))
             except Exception as e:
+                job_data['retry_count'] += 1
                 logger.error(f"Ошибка при обработке задачи: {str(e)}")
-                await msg.nak(delay=5)
+                await self.queue.put((job_data, msg))
 
-    async def _rate_limit(self):
-        # Получаю текущее время в секундах
-        current_time = time.time()
+    async def fetch_messages(self, sub):
+        try:
+            msgs = await sub.fetch(10, timeout=5)
+            logger.debug(f'Получено {len(msgs)}')
+            for msg in msgs:
+                job_data = json.loads(msg.data.decode())
 
-        # Вычисляю время, прошедшее с последней обработки задачи
-        elapsed_time = current_time - self.last_process_time
+                # Получаем уникальный идентификатор сообщения
+                msg_id = msg.headers.get("Nats-Msg-Id", None)
 
-        # Рассчитываю допустимое время между обработками
-        # duration - общее время, в течение которого могут быть обработаны max задач
-        allowed_time = self.rate_limit['duration'] / self.rate_limit['max']
+                # Проверяем, находится ли задача уже в процессе обработки
+                if msg_id in self.processing_jobs:
+                    logger.warning(f"Сообщение {msg_id} уже обрабатывается, пропускаем.")
+                    continue
 
-        # Если время, прошедшее с последней обработки, меньше допустимого времени
-        if elapsed_time < allowed_time:
-            # Засыпаем на оставшееся время, чтобы не превышать лимит
-            await asyncio.sleep(allowed_time - elapsed_time)
-
-        # Обновляем время последней обработки на текущее время
-        self.last_process_time = time.time()
+                await self.queue.put((job_data, msg))
+                self.processing_jobs.add(msg_id)  # Добавляем идентификатор в множество
+        except nats.errors.TimeoutError:
+            logger.error(f"Не удалось получить сообщения: истекло время ожидания.")
+        except Exception as e:
+            logger.error(f"Ошибка получения сообщений: {e}")
 
 
     async def start(self):
         subscriptions = []
         for priority in range(self.priorities):
-            topic = f"{self.topic_name}.*.{priority}"  # Подписка на формат с приоритетом
+            topic = f"{self.topic_name}.*.{priority}"
             try:
                 sub = await self.js.pull_subscribe(topic, durable=f"worker_group_{priority}")
                 logger.debug(f"Подписка на {topic} успешна: {sub}")
                 subscriptions.append(sub)
             except Exception as e:
                 logger.error(f"Ошибка подписки на {topic}: {e}")
-                continue
+
+        limiter = RateLimiter(self.rate_limit['max'], self.rate_limit['duration'])
 
         while True:
-            found_messages = False
-            for sub in subscriptions:
-                try:
-                    msgs = await sub.fetch(self.concurrency, timeout=5)
-                    logger.debug(f"Получено {len(msgs)} сообщений")
-                    if msgs:
-                        found_messages = True
-                        await self._rate_limit()  # Применяем лимит скорости
-                        for msg in msgs:
-                            logger.debug(f"Обработка сообщения {msg.data.decode()}")
-                            asyncio.create_task(self._process_task(msg))
-                    else:
-                        logger.info("Нет непрочитанных сообщений")
-                        await asyncio.sleep(1)
+            fetch_tasks = [self.fetch_messages(sub) for sub in subscriptions]
+            await asyncio.gather(*fetch_tasks)
+
+            while not self.queue.empty() and not self.semaphore.locked():
+                await limiter.check_limit()
+                if self.semaphore.locked():
+                    logger.info("Семафор занят, ожидаем...")
                     break
-                except Exception as e:
-                    logger.error(f"Ошибка получения сообщений: {e}")
-                    await asyncio.sleep(1)
-            if not found_messages:
-                logger.debug("Нет подходящих подписок")
+
+                job_data, msg = await self.queue.get()
+                asyncio.create_task(self._process_task(job_data, msg))
+                limiter.increment()
+
+            if self.queue.empty():
+                logger.info("Нет непрочитанных сообщений, ожидаем...")
                 await asyncio.sleep(1)
 
-# Пример использования
-async def process_job(msg):
-    job_data = json.loads(msg.data.decode())
-    logger.info(f"Обработка задачи: {job_data['id']}, данные: {job_data['data']}")
-    await asyncio.sleep(2)  # Имитация длительной работы
-    logger.info(f"Задача {job_data['id']} успешно обработана")
+async def process_job(job_data):
+    logger.info(f"Выполняется {job_data['name']} что-то делает...")
+    await asyncio.sleep(5)
 
 async def main():
     queue = Queue(topic_name="my_queue", priorities=3)
     await queue.connect()
 
-    jobs = [Job(queue_name="my_queue", name=f"task_{i}", data={"key": f"value_{i}"}) for i in range(10)]
-    await queue.addJobs(jobs, priority=0)
-
+    jobs = [Job(queue_name="my_queue", name=f"task_{i+1}", data={"key": f"value_{i+1}"}) for i in range(10)]
+    
+    await queue.addJobs(jobs)
+    
     worker = Worker(
         topic_name="my_queue",
         concurrency=3,
-        rate_limit={"max": 2, "duration": 5},
+        rate_limit={"max": 5, "duration": 30},
         processor_callback=process_job,
-        priorities=3
+        priorities=3,
+        max_retries=3
     )
     
     await worker.connect()
