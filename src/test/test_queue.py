495ab30c68a62e60b1311a100a28f89792406ce2
Kristina Shishkina <k.shishkina@darwinsoft.ru>
2024-09-24 13:40:39 +0300

    add: test_no_stream, test_close_connection, test_add_job_with_priority

--- src/test/test_queue.py
+++ src/test/test_queue.py
@@ -66,3 +66,57 @@ async def test_add_multiple_jobs():
     await queue.js.delete_stream(queue.topic_name)
     await queue.close()
 
+@pytest.mark.asyncio
+async def test_add_job_with_priority():
+    topic_name = "test_queue"
+    queue = Queue(topic_name=topic_name, priorities=3)
+    await queue.connect()
+
+    job_high = Job(queue_name="test_queue", name="high_priority_job", data={"key": "high_value"})
+    job_low = Job(queue_name="test_queue", name="low_priority_job", data={"key": "low_value"})
+
+    await queue.addJob(job_high, priority=0) 
+    await queue.addJob(job_low, priority=2)    
+
+    # Проверка высокого приоритета
+    sub_high = await queue.js.subscribe(f"{job_high.queue_name}.{job_high.name}.*")
+    message_high = await sub_high.next_msg()
+    assert message_high is not None
+    assert json.loads(message_high.data.decode()) == job_high.to_dict()
+
+    # Проверка низкого приоритета
+    sub_low = await queue.js.subscribe(f"{job_low.queue_name}.{job_low.name}.*")
+    message_low = await sub_low.next_msg()
+    assert message_low is not None
+    assert json.loads(message_low.data.decode()) == job_low.to_dict()
+
+    await queue.js.delete_stream(queue.topic_name)
+    await queue.close()
+
+@pytest.mark.asyncio
+async def test_no_stream():
+    topic_name = "non_existent_stream"
+    queue = Queue(topic_name=topic_name)
+    
+    await queue.connect()
+
+    job = Job(queue_name="test_queue", name="test_job", data={"key": "value"})
+    
+    with pytest.raises(Exception):
+        await queue.addJob(job, priority=1)
+
+    await queue.js.delete_stream(queue.topic_name)
+    await queue.close()
+
+@pytest.mark.asyncio
+async def test_close_connection():
+    topic_name = "test_queue"
+    queue = Queue(topic_name=topic_name)
+    
+    await queue.connect()
+    await queue.js.delete_stream(queue.topic_name)
+    await queue.close()
+    
+    assert queue.nc.is_closed
+
+
