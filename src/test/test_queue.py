ac9bf9466c67ca08abbad6824ab9b4a084a796a7
Kristina Shishkina <klikklok2017@mail.ru>
2024-12-03 11:22:36 +0300

    fix: delete import nats_queue

--- src/test/test_queue.py
+++ src/test/test_queue.py
@@ -5,7 +5,6 @@ from nats_queue.nats_queue import Queue
 from nats_queue.nats_job import Job
 from nats.aio.client import Client as NATS
 from nats.js.client import JetStreamContext as JetStream
-import nats_queue
 import nats
 
 
